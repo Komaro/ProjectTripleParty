2eec7b53139386bec5dfc50116f1fe313bda921e
komaro <komaro318@gmail.com>
2018-04-28 21:06:56 +0900

    4/28 update fadeinout

--- Assets/Intro_Sprite/title_fadeinout.cs
+++ Assets/Intro_Sprite/title_fadeinout.cs
@@ -9,48 +9,51 @@ public class title_fadeinout : MonoBehaviour {
 
     private Image fadeImage;
 
-    private float start = 0f;
-    private float end = 1f;
-    private float time = 0f;
-
-    private bool isPlaying = false;
-
+    private float start;
+    private float end;
+    private float time;
+    
 	// Use this for initialization
 	void Start () {
 
         fadeImage = GetComponent<Image>();
-
-        StartFadeAnim();
+        StartCoroutine(ImageFadeInOutLoop());
 	}
-	
-    public void StartFadeAnim()
+
+    IEnumerator ImageFadeInOutLoop()
     {
-        if(isPlaying == true)
-        {
-            return;
-        }
-
-        StartCoroutine("FadInOutLoop");
-    }
-
-    IEnumerable FadeInOutLoop()
-    {
-        isPlaying = true;
-
         Color color = fadeImage.color;
-        time = 0f;
-        color.a = Mathf.Lerp(start, end, time);
 
-        while(color.a < 1f)
+        while (true)
         {
-            time += Time.deltaTime / animTime;
+            start = 0f;
+            end = 1f;
+            time = 0f;
             color.a = Mathf.Lerp(start, end, time);
-            fadeImage.color = color;
 
-            yield return null;
+            while (color.a < 1f)
+            {
+                time += Time.deltaTime / animTime;
+                color.a = Mathf.Lerp(start, end, time);
+                fadeImage.color = color;
+
+                yield return null;
+            }
+
+            start = 1f;
+            end = 0f;
+            time = 0f;
+            color.a = Mathf.Lerp(start, end, time);
+            
+            while (color.a > 0f)
+            {
+                time += Time.deltaTime / animTime;
+                color.a = Mathf.Lerp(start, end, time);
+                fadeImage.color = color;
+
+                yield return null;
+            }
         }
-
-        isPlaying = false;
     }
 
 	// Update is called once per frame
