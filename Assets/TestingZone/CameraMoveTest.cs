0bb409533a08624e4c6a4c659c5cfae14c5ca097
komaro <komaro318@gmail.com>
2018-06-02 23:51:27 +0900

    18/06/02

---
+++ Assets/TestingZone/CameraMoveTest.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMoveTest : MonoBehaviour {
+
+    int speed = 10;
+    public Camera camera;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+    private void LateUpdate()
+    {
+        cameraZoom();
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        cameraMove();
+    }
+    
+    private void cameraMove()
+    {
+        float amtMove = speed * Time.smoothDeltaTime;
+
+        float translationX = Input.GetAxis("Horizontal");
+        float translationY = Input.GetAxis("Vertical");
+
+        transform.Translate(translationX * amtMove, 0, 0);
+        transform.Translate(0, translationY * amtMove, 0);
+    }
+
+    private void cameraZoom()
+    {
+        if(Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            camera.orthographicSize += 0.5f;
+        }
+
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            camera.orthographicSize -= 0.5f;
+        }
+    }
+}
