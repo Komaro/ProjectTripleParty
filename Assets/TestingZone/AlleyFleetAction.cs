f9b20f5a6fe2e709e1846f63280235f5ef63814b
Komaro <hhp318@naver.com>
2018-06-08 16:06:27 +0900

    18/6/8 update

---
+++ Assets/TestingZone/AlleyFleetAction.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets.TestingZone;
+
+public class AlleyFleetAction : MonoBehaviour {
+
+    // Alley Base Status
+    public float alleyRange;
+    public float targetingTime;
+
+    // Alley Move Status
+    public float speed;
+    public bool moveOrder = false;
+    public Vector3 moveDest;
+
+    // Alley Searching And Targeting Parameter
+    private List<GameObject> enemyList; // not GameObject => GameObject + distance value (objectDistance)
+
+    public GameObject priorityTarget;
+    public GameObject currentTarget;
+    
+
+	// Use this for initialization
+	void Start () {
+
+        // Temp initialization
+        alleyRange = 25f;
+        targetingTime = 5f;
+
+
+
+
+
+        enemyList = new List<GameObject>();
+
+        InvokeRepeating("SearchingEnemy", 0, 0.25f);
+        InvokeRepeating("selectTarget", 0, targetingTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public float targetDistance;
+
+    void SearchingEnemy()
+    {
+        foreach(GameObject target in EnemyInformation.getInstance().getEnemyList())
+        {
+            targetDistance = (target.transform.position - transform.position).sqrMagnitude;
+
+            if(targetDistance <= alleyRange && !enemyList.Contains(target))
+            {
+                Debug.Log("enemy add");
+                enemyList.Add(target);
+            }
+            else if(targetDistance > alleyRange && enemyList.Contains(target))
+            {
+                Debug.Log("enemy delete");
+                enemyList.Remove(target);
+            }
+        }
+    }
+
+    void selectTarget()
+    {
+        if (enemyList.Count == 0) return;
+
+        if(priorityTarget == null)
+        {
+            // foareach() // Min distance enemy select
+        }
+    }
+
+    void firingBattery()
+    {
+
+    }
+}
---
+++ Assets/TestingZone/EnemyInformation.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.TestingZone
+{
+    class EnemyInformation
+    {
+        public static EnemyInformation Instance;
+
+        private GameObject[] enemyList;
+
+        EnemyInformation()
+        {
+            enemyList = GameObject.FindGameObjectsWithTag("EnemyFleet");
+        }
+        
+        public static EnemyInformation getInstance()
+        {
+            if(Instance == null)
+            {
+                Instance = new EnemyInformation();
+            }
+
+            return Instance;
+        }
+
+        
+        
+        public GameObject[] getEnemyList()
+        {
+            return enemyList;
+        }
+    }
+}
