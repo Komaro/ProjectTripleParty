f9031d4142063f70bb175217009bd6c380ade280
komaro <komaro318@gmail.com>
2018-07-05 19:18:25 +0900

    18/7/5 update
    - Test fog of war

---
+++ Assets/TestingZone/CamDontClear.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamDontClear : MonoBehaviour {
+
+    [SerializeField]
+    private Camera camera;
+
+    void Awake()
+    {
+        if(camera == null)
+        {
+            camera = GetComponent<Camera>();
+
+            Initalize();
+        }
+    }
+
+    public void Initalize()
+    {
+        camera.clearFlags = CameraClearFlags.Color;
+    }
+
+    private void OnPostRender()
+    {
+        camera.clearFlags = CameraClearFlags.Nothing;
+    }
+}
